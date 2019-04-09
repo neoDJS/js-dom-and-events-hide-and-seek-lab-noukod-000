aec595c1f1b7cdfd8c873f56133ce43e6b2b1cd9
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-09 20:23:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-  return document.getElementById('nested').getElementsByClassName('target');
+  return document.querySelector('#nested .target');
 }
 
 function increaseRankBy(n){
